00f6ae2a890b7a68849258c116febca64c985e0b
Big Sam <stosin525@gmail.com>
2025-06-12 21:53:10 +0100

    feat: implement project detail page layout, styling, and route setup

---
+++ src/components/project_detail/Heading.jsx
@@ -0,0 +1,28 @@
+import Button from "../ui/Button";
+
+const Heading = ({ title, note, live, github }) => {
+  return (
+    <>
+      <div className="space-y-4 w-full md:w-3/6">
+        <h2 className="text-4xl text-[rgb(var(--colot-text))] font-extrabold">
+          {title}
+          <span className="text-5xl text-[rgb(var(--color-brand))]">.</span>
+        </h2>
+        <p className="text-base text-[rgb(var(--color-muted))] font-medium">
+          {note}
+        </p>
+      </div>
+
+      <div className="grow flex md:justify-end space-x-3 w-full md:w-3/6">
+        <a href={live} target="_blank" rel="noopener noreferrer">
+          <Button type="button" children="Live Website" className="" />
+        </a>
+        <a href={github} target="_blank" rel="noopener noreferrer">
+          <Button type="button" children="Github" variant="outline" />
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Heading;
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,13 +11,13 @@ const Button = ({
   ...props
 }) => {
   const baseClasses =
-    "px-6 py-2 rounded-md shadow-md font-chakra font-semibold text-center flex justify-center transition cursor-pointer flex items-center";
+    "px-6 py-2 rounded-md shadow-md font-chakra font-semibold text-center flex justify-center transition cursor-pointer flex items-center border-2";
 
   const variants = {
     primary:
-      "text-sm bg-[rgb(var(--color-brand))] hover:bg-[rgb(var(--color-brand-light))] text-white ",
+      "text-sm bg-[rgb(var(--color-brand))] border-[rgb(var(--color-brand))] hover:bg-[rgb(var(--color-brand-light))] hover:border-[rgb(var(--color-brand-light))] text-white ",
     outline:
-      "bg-white border-2 border-[rgb(var(--color-text))] hover:bg-[rgb(var(--color-text))] text-[rgb(var(--color-text))] hover:text-white text-sm",
+      "bg-white border-[rgb(var(--color-text))] hover:bg-[rgb(var(--color-text))] text-[rgb(var(--color-text))] hover:text-white text-sm",
   };
 
   const finalClass = clsx(baseClasses, variants[variant], className);
