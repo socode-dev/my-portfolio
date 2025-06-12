d5e27b82b1e55e991e9c492db17f711ff2ffb107
Big Sam <stosin525@gmail.com>
2025-06-12 12:24:39 +0100

    feat: Projects showcase in projects page

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,13 +11,13 @@ const Button = ({
   ...props
 }) => {
   const baseClasses =
-    "px-6 py-2 rounded-md shadow-md font-poppins font-medium text-center flex justify-center transition cursor-pointer flex items-center";
+    "px-6 py-2 rounded-md shadow-md font-chakra font-semibold text-center flex justify-center transition cursor-pointer flex items-center";
 
   const variants = {
     primary:
-      "text-xs bg-[rgb(var(--color-brand))] hover:bg-[rgb(var(--color-brand-light))] text-white ",
+      "text-sm bg-[rgb(var(--color-brand))] hover:bg-[rgb(var(--color-brand-light))] text-white ",
     outline:
-      "bg-white border border-[rgb(var(--color-text))] hover:bg-[rgb(var(--color-text))] hover:text-white text-xs",
+      "bg-white border-2 border-[rgb(var(--color-text))] hover:bg-[rgb(var(--color-text))] text-[rgb(var(--color-text))] hover:text-white text-sm",
   };
 
   const finalClass = clsx(baseClasses, variants[variant], className);
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,11 @@
 import { motion } from "framer-motion";
+import SectionWrapper from "../components/layouts/SectionWrapper";
+import { projectData } from "../data/projectData";
+import AnimatedProject from "../components/project/AnimatedProject";
 
 const Projects = () => {
+  const projects = [...projectData].reverse();
+
   return (
     <motion.main
       initial={{ opacity: 0, y: "-100%" }}
@@ -8,7 +13,22 @@ const Projects = () => {
       exit={{ opacity: 0, y: "-100%" }}
       transition={{ duration: 0.4, ease: "easeOut" }}
     >
-      This is Projects Page
+      <SectionWrapper className="space-y-5">
+        <h2 className="text-5xl text-[rgb(var(--color-text))] font-extrabold">
+          My <span className="text-[rgb(var(--color-brand))]">Best</span>{" "}
+          Creations
+        </h2>
+        <p className="text-base text-[rgb(var(--color-muted))] font-medium">
+          Each project you see here started with a blank file &mdash; and turned
+          into something functional, responsive, and meaningful
+        </p>
+      </SectionWrapper>
+
+      <SectionWrapper className="space-y-8">
+        {projects.map((project, i) => (
+          <AnimatedProject key={project.id} project={project} />
+        ))}
+      </SectionWrapper>
     </motion.main>
   );
 };
